58b965d2fb007424ec35314ed3e69e2cf5479674
Michel Rodrigues <sobreira.michel@gmail.com>
2024-02-03 20:08:53 -0300

    test: unit test pages

---
+++ src/pages/__tests__/home.test.tsx
@@ -0,0 +1,46 @@
+import { MOCK_RESPONSE_CATEGORY } from '@/services/category/__tests__/mock';
+import { MOCK_RESPONSE_SEARCH } from '@/services/search/__tests__/mock';
+
+import { mockGetRequest } from '@test/mocks/server';
+import { customRender, screen, waitFor } from '@test/utils/render';
+import HomePage from '@/pages';
+
+describe('Pages - Home', () => {
+  beforeEach(() => {
+    const siteId = 'MLB';
+
+    mockGetRequest({
+      endpoint: `/sites/${siteId}/categories`,
+      response: MOCK_RESPONSE_CATEGORY,
+    });
+
+    mockGetRequest({
+      endpoint: `/sites/${siteId}/search`,
+      response: {
+        ...MOCK_RESPONSE_SEARCH,
+        results: Array.from({ length: 10 }, (_, index) => ({
+          ...MOCK_RESPONSE_SEARCH.results[0],
+          id: `${index}`,
+          title: `Title ${index}`,
+        })),
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render the page Home', async () => {
+    customRender(<HomePage />);
+    expect(screen.getByTestId('layout')).toBeInTheDocument();
+    expect(screen.getByTestId('header')).toBeInTheDocument();
+
+    await waitFor(() => screen.getByTestId('sidebar'));
+    expect(screen.getByTestId('footer')).toBeInTheDocument();
+
+    await waitFor(() => screen.getByTestId('home-screen'));
+    expect(screen.getByText('Title 1')).toBeInTheDocument();
+    expect(screen.queryByText('Promoções')).toBeInTheDocument();
+  });
+});
---
+++ src/pages/__tests__/search.test.tsx
@@ -0,0 +1,68 @@
+import { MOCK_RESPONSE_CATEGORY } from '@/services/category/__tests__/mock';
+import { MOCK_RESPONSE_SEARCH } from '@/services/search/__tests__/mock';
+
+import { mockGetRequest } from '@test/mocks/server';
+import {
+  act,
+  customRender,
+  customRenderHook,
+  screen,
+  waitFor,
+} from '@test/utils/render';
+import SearchPage from '@/pages/buscar/[search]';
+import { useSearchItem } from '@/services/search';
+
+describe('Pages - Search', () => {
+  const siteId = 'MLB';
+
+  beforeEach(() => {
+    mockGetRequest({
+      endpoint: `/sites/${siteId}/categories`,
+      response: MOCK_RESPONSE_CATEGORY,
+    });
+
+    mockGetRequest({
+      endpoint: `/sites/${siteId}/search`,
+      response: MOCK_RESPONSE_SEARCH,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render the page Search', async () => {
+    const searchTerm = 'product 1';
+
+    const { result } = customRenderHook(() =>
+      useSearchItem({
+        siteId,
+        limit: 10,
+        offset: 0,
+        search: searchTerm,
+      })
+    );
+
+    await act(async () => {
+      await waitFor(() => result.current.isSuccess);
+    });
+
+    expect(result.current.data).not.toBe(undefined);
+
+    if (!result.current.data) {
+      return;
+    }
+
+    customRender(<SearchPage data={result.current.data} search={searchTerm} />);
+    expect(screen.getByTestId('layout')).toBeInTheDocument();
+    expect(screen.getByTestId('header')).toBeInTheDocument();
+
+    await waitFor(() => screen.getByTestId('sidebar'));
+    expect(screen.getByTestId('footer')).toBeInTheDocument();
+
+    expect(screen.getByText(searchTerm)).toBeInTheDocument();
+    expect(
+      screen.getByText(`${result.current.data?.results.length} resultados`)
+    ).toBeInTheDocument();
+  });
+});
