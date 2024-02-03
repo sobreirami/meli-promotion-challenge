763af9ee15dae779470a067e1d44d0c9163eaf29
Michel Rodrigues <sobreira.michel@gmail.com>
2024-02-03 14:13:56 -0300

    test: snapshot carousel component

---
+++ src/ui/__tests__/carousel.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@test/utils/render';
+import { Carousel } from '@/ui/carousel';
+
+describe('UI - Carousel Snapshot', () => {
+  it('should match snapshot with default props', () => {
+    const items = [
+      <div key={0}>Item 1</div>,
+      <div key={1}>Item 2</div>,
+      <div key={2}>Item 3</div>,
+    ];
+
+    const { container } = render(<Carousel items={items} />);
+    expect(container).toMatchSnapshot();
+  });
+});
